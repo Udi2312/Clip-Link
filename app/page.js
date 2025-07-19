27d86b2edf62b1c485d7af8691d21327b2fb816c
Udit Bansal <Udit.ban2312@gmail.com>
2025-07-19 15:54:57 +0530

    commit

--- app/page.js
+++ app/page.js
@@ -1,20 +1,25 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main>
+    <main className="bg-purple-100">
    <section className="grid grid-cols-2 h-[50vh]">
-    <div className="bg-green-50">
-      <p>
+    <div className="flex flex-col gap-4 items-center justify-center">
+      <p className="text-3xl font-bold">
         The best URL shortener in the market.
       </p>
-      <p>
+      <p className="px-56 text-center">
         We are the most straightforward URL shortener in the world
       </p>
+      <div className='flex gap-3 justify-start'>
+                <Link href="/generate"><button className='bg-purple-500 shadow-lg p-3 rounded-lg font-bold py-1 text-white'>Try Now</button></Link>
+                <Link href="/github"><button className='bg-purple-500 shadow-lg p-3 rounded-lg font-bold py-1 text-white'>Github</button></Link>
+            </div>
     </div>
 
-    <div className="bg-red-500 flex justify start relative">
-      <Image alt="an image of vector" src = {"/vector.jpg"} fill={true}/>
+    <div className=" flex justify start relative">
+      <Image className="mix-blend-darken" alt="an image of vector" src = {"/vector.jpg"} fill={true}/>
     </div>
    </section>
     </main>
---
+++ app/shorten/page.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Shorten = () => {
+  return (
+    <div>
+    <h1>
+        Generate your short URLs.
+    </h1>
+    <div>
+        
+    </div>
+    </div>
+  )
+}
+
+export default Shorten
