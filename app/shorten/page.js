a9a1ce1f3da17f8b6d25c8745962313e8cfd8beb
Udit Bansal <Udit.ban2312@gmail.com>
2025-07-19 22:05:31 +0530

    commit

--- app/shorten/page.js
+++ app/shorten/page.js
@@ -1,13 +1,24 @@
+'use client'
 import React from 'react'
+import { useState } from 'react'
 
 const Shorten = () => {
+    const [url, seturl] = useState("")
+    const [shorturl, setshorturl] = useState("")
+    const [generated, setgenerated] = useState(false)
+
+    const handleChange = ()=>{
+
+    }
   return (
-    <div>
+    <div className='mx-auto max-w-lg'>
     <h1>
         Generate your short URLs.
     </h1>
     <div>
-        
+        <input type="text" placeholder='Enter your long URL here' onChange={handleChange} />
+        <input type="text" placeholder='Enter your prefered short URL text' className='p-4 focus: outline-purple-600' onChange={handleChange} />
+        <button>Generate</button>
     </div>
     </div>
   )
